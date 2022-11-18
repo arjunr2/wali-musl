77ae3b1f943a21536fcb1be8d47275f1b9dbcc93
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-11-18 14:27:33 -0500

    Improve wali def generator

--- helpers/gen_wali_defs.py
+++ helpers/gen_wali_defs.py
@@ -1,18 +1,71 @@
 import re
 
 def main():
-    input_file = open("syscallnrs.h", "r").readlines()
+    input_file = open("syscallnrs_x86_64.h", "r").readlines()
     
     pattern = re.compile(r"__NR_(\S*)");
 
     defs = []
-    with open("wali_syscall_defs.txt", "w") as f:
+    with open("syscall_defs.txt", "w") as g:
         for line in input_file:
             match = pattern.search(line)
             if match:
                 defs.append(match.group(1))
+        g.writelines([x+'\n' for x in defs])
 
-        f.writelines(["WALI_SYSCALL_DEF ({});\n".format(x) for x in defs])
+
+    wali_defs = []
+    PL_VAL = 12345
+    append_fn = lambda names, args: [wali_defs.append("WALI_SYSCALL_DEF ({}, {});".format(name, ','.join(args))) for name in names]
+    
+    with open("wali_syscall_defs.txt", "w") as f:
+        for x in defs:
+            map_val = mapping.get(x, [PL_VAL, ()])
+            args = map_val[1]
+
+            print(x)
+            print(map_val)
+            print(args)
+            append_fn([x], args)
+
+            if len(map_val) == 3:
+                append_fn(map_val[2], args)
+
+
+        f.writelines('\n'.join(wali_defs))
+
+
+INT="int"
+_CHAR="char*"
+SIZE_T="size_t"
+_VOID="void*"
+LONG="long"
+
+mapping = {
+    "read"      :   [0, (INT,    _CHAR,  SIZE_T )],
+    "write"     :   [1, (INT,    _CHAR,  SIZE_T )],
+    "open"      :   [2, (_CHAR,  INT,    INT    )],
+    "close"     :   [3, (INT                    )],
+    "stat"      :   [4, (_CHAR,  _VOID          )],
+    "fstat"     :   [5, (INT,    _VOID          )],
+    "lstat"     :   [6, (_CHAR,  _VOID          )],
+    "poll"      :   [7, (_VOID,  INT,    LONG   )],
+    "lseek"     :   [8, (INT,    LONG,   INT    )],
+    "mmap"      :   [9, (LONG,   LONG,   LONG,   LONG,   LONG,   LONG)],
+
+    "mprotect"  :   [10,    (LONG,   SIZE_T, LONG)],
+    "rt_sigprocmask"    :   [14,    (INT,    _VOID,  _VOID,  SIZE_T)],
+
+    "fcntl"     :   [72,    (INT,    INT,    LONG)],
+
+    # Mode_t check
+    "fchmodat"  :   [268,   (INT,    _CHAR,  INT)],
+    "newfstatat":   [262,   (INT,    _CHAR,  _VOID,  INT),  ("fstatat",)],
+    "futex"     :   [202,   (_VOID,  INT,    INT,    _VOID,  _VOID,  INT)],
+
+    "fadvise64" :   [221,   (INT,    LONG,   SIZE_T, INT),  ("fadvise",)]
+
+}
 
 if __name__ == '__main__':
     main()
