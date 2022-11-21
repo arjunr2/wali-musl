b7780da22a819b57cc441e68b09840bb465e9dc0
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-11-21 14:11:41 -0500

    Added arg substitition for undef types

--- helpers/gen_wali_defs.py
+++ helpers/gen_wali_defs.py
@@ -3,7 +3,16 @@ import pandas as pd
 import csv
 
 BASIC_TYPES = ["int", "char", "long", "void", "..."]
-COMPLEX_TYPES = ["off_t"]
+COMPLEX_TYPES = {
+    "off_t": "long", 
+    "size_t": "long", 
+    "mode_t": "int",
+    "nfds_t": "int",
+    "socklen_t": "long",
+    "uid_t": "int",
+    "pid_t": "int",
+    "gid_t": "int"
+}
 
 
 def gen_syscall_list():
@@ -22,6 +31,23 @@ def gen_syscall_list():
     return defs
 
 
+def gen_args(x):
+    if not x['# Args']:
+        return []
+
+    args = [x["a"+str(i+1)] for i in range(int(x['# Args']))]
+    arg_sub = []
+    print(args)
+    for arg in args:
+        if arg[-1] == '*': 
+            arg_sub.append(arg[:-1]+'*' if arg[:-1] in BASIC_TYPES else "void*")
+        elif arg not in BASIC_TYPES:
+            arg_sub.append(COMPLEX_TYPES.get(arg, "UNDEF"))
+        else:
+            arg_sub.append(arg)
+    
+    return arg_sub
+
 
 def main():
     out_file = "wali_syscall_defs.txt"
@@ -34,11 +60,12 @@ def main():
     syscall_info = df_dict #[i for i in df_dict if i['# Args'] != ""]
     
     append_fn = lambda name, args: wali_defs.append("WALI_SYSCALL_DEF ({}, {});".format(name, ','.join(args)))
-    gen_args = lambda x: [] if not x['# Args'] else [x["a"+str(i+1)] for i in range(int(x['# Args']))]
+    #gen_args = lambda x: [] if not x['# Args'] else [x["a"+str(i+1)] for i in range(int(x['# Args']))]
 
     wali_defs = []
     for item in syscall_info:
         args = gen_args(item)
+        print("{}: {}".format(item['Syscall'], args))
         append_fn(item['Syscall'], args)
         for i in filter(None, item['Aliases'].split(',')):
             append_fn(i, args)
@@ -47,7 +74,6 @@ def main():
     with open(out_file, "w") as f:
         f.writelines('\n'.join(wali_defs))
 
-    print(wali_defs)
 
 
 if __name__ == '__main__':
