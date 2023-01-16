ba15a0f289b659d1adfed26fb2dc926c42bc9864
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-01-16 10:47:23 +0000

    makefile improved to generate tests; added fstat2 and sizes test

--- tests/compile-wali.sh
+++ tests/compile-wali.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 sysroot_dir=../sysroot
-
 crtfile=startup/crt1.o 
-file=$(basename $1 .c)
 
-clang --target=wasm32 --sysroot=$sysroot_dir $file.c -S -emit-llvm -o $file.ll
-llc $file.ll -filetype=obj -o $file.wasm
-wasm2wat $file.wasm -o $file.wat
-wasm-ld --no-entry --allow-undefined -L$sysroot_dir/lib $file.wasm $crtfile -lc -o ${file}_link.wasm
-wasm2wat ${file}_link.wasm -o ${file}_link.wat
+outdir=.
+
+while getopts "vo:" OPT; do
+  case $OPT in
+    v) verbose=-v;;
+    o) outdir=$OPTARG;;
+    *) 
+      echo "Incoorect opt provided"
+      exit 1 ;;
+  esac
+done
+cfile=${@:$OPTIND:1}
+outbase=$outdir/$(basename $cfile .c)
+
+clang --target=wasm32 --sysroot=$sysroot_dir $cfile -S -emit-llvm -o $outbase.ll $verbose
+llc $outbase.ll -filetype=obj -o $outbase.wasm
+wasm2wat $outbase.wasm -o $outbase.wat
+wasm-ld --no-entry --allow-undefined -L$sysroot_dir/lib $outbase.wasm $crtfile -lc -o ${outbase}_link.wasm
+wasm2wat ${outbase}_link.wasm -o ${outbase}_link.wat
