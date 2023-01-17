1756b713b5f859d0fa8d4017e0fb60fb0769ef19
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-01-17 12:03:05 -0500

    Remove VDSO support for now

--- arch/x86_64/syscall_arch.h
+++ arch/x86_64/syscall_arch.h
@@ -463,11 +463,11 @@ WALI_SYSCALL_DEF (rseq, );
 
 
 
-#define VDSO_USEFUL
-#define VDSO_CGT_SYM "__vdso_clock_gettime"
-#define VDSO_CGT_VER "LINUX_2.6"
-#define VDSO_GETCPU_SYM "__vdso_getcpu"
-#define VDSO_GETCPU_VER "LINUX_2.6"
+//#define VDSO_USEFUL
+//#define VDSO_CGT_SYM "__vdso_clock_gettime"
+//#define VDSO_CGT_VER "LINUX_2.6"
+//#define VDSO_GETCPU_SYM "__vdso_getcpu"
+//#define VDSO_GETCPU_VER "LINUX_2.6"
 
 #define IPC_64 0
 
