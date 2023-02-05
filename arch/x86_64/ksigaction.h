71304a015742627ae4f5c9ee0a1f39c016b4c421
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-05 14:55:06 -0500

    Pack k_sigaction struct

--- arch/x86_64/ksigaction.h
+++ arch/x86_64/ksigaction.h
@@ -1,11 +1,14 @@
 #include <features.h>
 
-struct k_sigaction {
+struct __attribute__((packed)) k_sigaction {
 	void (*handler)(int);
 	unsigned long flags;
 	void (*restorer)(void);
 	unsigned mask[2];
 };
 
-hidden void __restore_rt();
+hidden void __restore_rt() __attribute((
+  __import_module__("wali"),
+  __import_name__("__restore_rt")
+));
 #define __restore __restore_rt
