788f2084d837a8cd6fd3dc8887ca1a8d6b5238bd
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-05 15:22:52 -0500

    Substitute errno on process level until fixing a thread model

--- src/errno/__errno_location.c
+++ src/errno/__errno_location.c
@@ -1,9 +1,12 @@
 #include <errno.h>
+#include <stdio.h>
 #include "pthread_impl.h"
 
 int *__errno_location(void)
 {
-	return &__pthread_self()->errno_val;
+	//return &__pthread_self()->errno_val;
+  printf("-- WARNING -- Setting Errno\n");
+  return &__errno_process;
 }
 
 weak_alias(__errno_location, ___errno_location);
--- src/include/errno.h
+++ src/include/errno.h
@@ -7,6 +7,7 @@
 __attribute__((const))
 #endif
 hidden int *___errno_location(void);
+int __errno_process = 0;
 
 #undef errno
 #define errno (*___errno_location())
