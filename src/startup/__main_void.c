1fbec23ab1308cf2525a142be69397474ee4eda8
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-05-12 15:48:16 -0400

    Added wali get cl args

--- src/startup/__main_void.c
+++ src/startup/__main_void.c
@@ -1,18 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int __cl_get_argc(void) __attribute ((
+  __import_module__("wali"),
+  __import_name__("__cl_get_argc")
+));
+
+int __cl_get_argv_len(int) __attribute ((
+  __import_module__("wali"),
+  __import_name__("__cl_get_argv_len")
+));
+
+int __cl_copy_argv(char*, int) __attribute ((
+  __import_module__("wali"),
+  __import_name__("__cl_copy_argv")
+));
+
 /* User Main Function with args that clang generates */
 int __main_argc_argv(int argc, char *argv[]);
 
+/* Main void if not defined by clang (i.e, if the argument main is used) */
 __attribute__((__weak__, nodebug))
 int __main_void(void) {
-  const char *test_str = "hello world";
-  int argc = 5;
+  int argc = __cl_get_argc();
   char **argv = (char**) malloc(argc * sizeof(char*));
+  if (argv == NULL) {
+    return 1;
+  }
   for (int i = 0; i < argc; i++) {
-    char *arg = (char*) malloc(80);
-    sprintf(arg, "Arg%d", i);
-    argv[i] = arg;
+    int argv_len = __cl_get_argv_len(i);
+    argv[i] = (char *) malloc(argv_len);
+    if (argv[i] == NULL) {
+      return 1;
+    }
+    __cl_copy_argv(argv[i], i);
   }
   return __main_argc_argv(argc, argv);
 }
