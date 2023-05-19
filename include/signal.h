41bd808e9f2bd9ca986ec6c64f8dd07ed2fd1de7
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-05-19 18:05:47 -0400

    Change SIG_IGN to -2 for wasm tables

--- include/signal.h
+++ include/signal.h
@@ -282,7 +282,7 @@ int sigandset(sigset_t *, const sigset_t *, const sigset_t *);
 
 #define SIG_ERR  ((void (*)(int))-1)
 #define SIG_DFL  ((void (*)(int)) 0)
-#define SIG_IGN  ((void (*)(int)) 1)
+#define SIG_IGN  ((void (*)(int))-2)
 
 typedef int sig_atomic_t;
 
