695d1333347cb5c64bc506e72ead6a2109528490
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-09-02 13:30:11 -0400

    [Breaking] Rename init/deinit imports for wali, and export initializer/deinitalizer functions

--- crt/crt1-command.c
+++ crt/crt1-command.c
@@ -3,13 +3,15 @@
 
 extern void __wasm_init_tp(void);
 
-void __wali_call_ctors(void) __attribute((
+// Engine-internal initialization
+void __wali_init(void) __attribute((
   __import_module__("wali"),
-  __import_name__("__call_ctors")
+  __import_name__("__init")
 ));
-void __wali_call_dtors(void) __attribute((
+// Engine-internal deinitialization
+void __wali_deinit(void) __attribute((
   __import_module__("wali"),
-  __import_name__("__call_dtors")
+  __import_name__("__deinit")
 ));
 
 void __wali_proc_exit(int) __attribute((
@@ -19,29 +21,33 @@ void __wali_proc_exit(int) __attribute((
 
 extern int __main_void(void);
 
-__attribute__((export_name("_start")))
-void _start(void) {
+// __wali_init must be run before any main or other exported functions
+__attribute__((export_name("__wali_startup")))
+void __wali_startup(void) {
   static volatile int started = 0;
   if (a_cas(&started, 0, 1)) {
     __builtin_trap();
   }
   
-  // The linker synthesizes this to call constructors.
-  __wali_call_ctors();
-
+  __wali_init();
   __wasm_init_tp();
-
-  // Initialize environment variables from WALI
+  // Initialize WALI environment variables
   init_env();
+}
 
+// __wali_cleanup must be run before exiting to cleanup any dangling state
+__attribute__((export_name("__wali_cleanup")))
+void __wali_cleanup(void) {
+  __wali_deinit();
+}
+
+__attribute__((export_name("_start")))
+void _start(void) {
+  __wali_startup();
   // Call `__main_void` which will either be the application's zero-argument
   // `__main_void` function or a libc routine which obtains the command-line
   // arguments and calls `__main_argv_argc`.
   int r = __main_void();
-
-  // Call atexit functions, destructors, stdio cleanup, etc.
-  __wali_call_dtors();
-
+  __wali_cleanup();
   __wali_proc_exit(r);
-
 }
