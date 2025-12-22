d4b2bf26c9f66367ccf58010c33ee7ec900a4fda
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-12-22 14:22:11 -0500

    Moved startup/cleanup outside crt for easy export

--- crt/crt1-command.c
+++ crt/crt1-command.c
@@ -1,45 +1,11 @@
-#include "atomic.h"
-#include "init_env.h"
-
-extern void __wasm_init_tp(void);
-
-// Engine-internal initialization
-void __wali_init(void) __attribute((
-  __import_module__("wali"),
-  __import_name__("__init")
-));
-// Engine-internal deinitialization
-void __wali_deinit(void) __attribute((
-  __import_module__("wali"),
-  __import_name__("__deinit")
-));
+extern int __main_void(void);
+extern void __wali_startup(void);
+extern void __wali_cleanup(void);
 
 void __wali_proc_exit(int) __attribute((
   __import_module__("wali"),
   __import_name__("__proc_exit")
 ));
-
-extern int __main_void(void);
-
-// __wali_init must be run before any main or other exported functions
-__attribute__((export_name("__wali_startup")))
-void __wali_startup(void) {
-  static volatile int started = 0;
-  if (a_cas(&started, 0, 1)) {
-    __builtin_trap();
-  }
-  
-  __wali_init();
-  __wasm_init_tp();
-  // Initialize WALI environment variables
-  init_env();
-}
-
-// __wali_cleanup must be run before exiting to cleanup any dangling state
-__attribute__((export_name("__wali_cleanup")))
-void __wali_cleanup(void) {
-  __wali_deinit();
-}
 
 __attribute__((export_name("_start")))
 void _start(void) {
--- src/startup/__main_void.c
+++ src/startup/__main_void.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "init_env.h"
+#include "atomic.h"
 
+/* Command Line Argument handling for WALI */
 int __cl_get_argc(void) __attribute ((
   __import_module__("wali"),
   __import_name__("__cl_get_argc")
@@ -15,6 +18,44 @@ int __cl_copy_argv(char*, int) __attribute ((
   __import_module__("wali"),
   __import_name__("__cl_copy_argv")
 ));
+
+// Engine-internal initialization
+void __wali_init(void) __attribute((
+  __import_module__("wali"),
+  __import_name__("__init")
+));
+// Engine-internal deinitialization
+void __wali_deinit(void) __attribute((
+  __import_module__("wali"),
+  __import_name__("__deinit")
+));
+
+extern void __wasm_init_tp(void);
+
+/* Export startup methods */
+// __wali_init must be run before any main or other exported functions
+//
+// This is called by default in `crt`, but is exported for users to 
+// explicitly call if using -nostartfiles
+void __wali_startup(void) {
+  static volatile int started = 0;
+  if (a_cas(&started, 0, 1)) {
+    __builtin_trap();
+  }
+  __wali_init();
+  __wasm_init_tp();
+  // Initialize WALI environment variables
+  init_env();
+}
+
+// __wali_cleanup must be run before exiting to cleanup any dangling state
+//
+// This is called by default in `crt`, but is exported for users to 
+// explicitly call if using -nostartfiles
+void __wali_cleanup(void) {
+  __wali_deinit();
+}
+
 
 /* User Main Function with args that clang generates */
 int __main_argc_argv(int argc, char *argv[]);
